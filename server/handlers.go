eb6f4725fb83a23131be2ad63af5c87dd3bf5edf
nicourrrn <s57111702@gmail.com>
2024-11-18 17:32:56 +0200

    Add SendData and Clients

--- server/handlers.go
+++ server/handlers.go
@@ -9,6 +9,7 @@ import (
 
 type ServerState struct {
 	clients []Client
+	updates []Data
 }
 
 func (state *ServerState) Registration(w http.ResponseWriter, r *http.Request) {
@@ -24,10 +25,19 @@ func (state *ServerState) Registration(w http.ResponseWriter, r *http.Request) {
 }
 
 func (state *ServerState) SendData(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var data Data
+	err := decoder.Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Data received: ", data)
+	state.updates = append(state.updates, data)
 }
 
 func (state *ServerState) Clients(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(state.clients)
+	w.Header().Set("Content-Type", "application/json")
 }
 
 func (state *ServerState) Updates(w http.ResponseWriter, r *http.Request) {
